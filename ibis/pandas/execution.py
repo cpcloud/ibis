dcd9cd896d752fd68dee8a50b0332e1347dbf483
Phillip Cloud <cpcloud@gmail.com>
2017-05-17 18:42:13 -0400

    Execution stubs

--- ibis/pandas/execution.py
+++ ibis/pandas/execution.py
@@ -1,3 +1,8 @@
+import numbers
+import operator
+
+import six
+
 import toolz
 
 import pandas as pd
@@ -8,9 +13,79 @@ import ibis.expr.operations as ops
 from ibis.pandas.dispatch import execute, execute_node
 
 
+@execute_node.register(ir.Literal)
+def execute_node_literal(op):
+    return op.value
+
+
 @execute_node.register(ops.TableColumn, pd.DataFrame)
 def execute_table_column_dataframe(op, data, scope=None):
     return data[op.name]
+
+
+@execute_node.register(ops.Selection, pd.DataFrame)
+def execute_selection_dataframe(op, data, scope=None):
+    pass
+
+
+@execute_node.register(ops.Aggregation, pd.DataFrame)
+def execute_aggregation_dataframe(op, data, scope=None):
+    pass
+
+
+@execute_node.register(ops.Reduction, pd.Series)
+def execute_reduction_series(op, data, scope=None):
+    return getattr(data, type(op).__name__)()
+
+
+@execute_node.register(ops.Reduction, pd.Series, pd.Series)
+def execute_masked_reduction_series(op, data, mask, scope=None):
+    return getattr(data[mask], type(op).__name__)()
+
+
+_JOIN_TYPES = {
+    ops.LeftJoin: 'left',
+    ops.InnerJoin: 'inner',
+    ops.OuterJoin: 'outer',
+}
+
+
+@execute_node.register(ops.Join, pd.DataFrame, pd.DataFrame)
+def execute_join(op, left, right, scope=None):
+    try:
+        how = _JOIN_TYPES[type(op)]
+    except KeyError:
+        raise NotImplementedError('{} not supported'.format(type(op).__name__))
+
+    left_on = []
+    right_on = []
+    return pd.merge(left, right, how=how, left_on=left_on, right_on=right_on)
+
+
+_BINARY_OPERATIONS = {
+    ops.Greater: operator.gt,
+    ops.Less: operator.lt,
+    ops.LessEqual: operator.le,
+    ops.GreaterEqual: operator.ge,
+    ops.Equals: operator.eq,
+    ops.NotEquals: operator.ne,
+
+    ops.And: operator.and_,
+    ops.Or: operator.or_,
+
+    ops.Add: operator.add,
+    ops.Subtract: operator.sub,
+    ops.Multiply: operator.mul,
+    ops.Divide: operator.truediv,
+    ops.Power: operator.pow,
+}
+
+
+@execute_node.register(
+    ops.BinaryOp, pd.Series, (pd.Series, numbers.Real) + six.string_types
+)
+def execute_binary_operation_series(op, left, right, scope=None):
+    return _BINARY_OPERATIONS[type(op)](left, right)
 
 
 def hashable(obj):
@@ -58,4 +133,12 @@ def execute_with_scope(expr, scope):
 
 @execute.register(ir.Expr)
 def execute_without_scope(expr):
-    return execute(expr, toolz.merge(find_data(expr)))
+    scope = toolz.merge(find_data(expr))
+    if not scope:
+        raise ValueError('No data sources found')
+    return execute(expr, scope)
+
+
+@execute.register(ir.ScalarExpr)
+def execute_literal(literal):
+    return execute_node(literal.op())
