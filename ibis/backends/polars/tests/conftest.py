aa09c88c8dd85ea38318e0e0ec19e125b43bc3e1
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-06-26 09:41:45 -0400

    test(polars): tpc setup

--- ibis/backends/polars/tests/conftest.py
+++ ibis/backends/polars/tests/conftest.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, Literal
 
 import numpy as np
 import pytest
+import sqlglot as sg
 
 import ibis
 from ibis.backends.conftest import TEST_TABLES
@@ -14,6 +15,9 @@ from ibis.backends.tests.data import array_types, struct_types, topk, win
 class TestConf(BackendTest):
     supports_structs = True
     supports_json = False
+    supports_tpch = True
+    supports_tpcds = True
+
     reduction_tolerance = 1e-3
     stateful = False
     deps = ("polars",)
@@ -41,6 +45,31 @@ class TestConf(BackendTest):
         return super().assert_series_equal(
             left, right, *args, **kwargs, check_dtype=check_dtype
         )
+
+    def _load_tpc(self, *, suite, scale_factor):
+        con = self.connection
+        schema = f"tpc{suite}"
+        parquet_dir = self.data_dir.joinpath(schema, f"sf={scale_factor}", "parquet")
+        assert parquet_dir.exists(), parquet_dir
+        for path in parquet_dir.glob("*.parquet"):
+            table_name = path.with_suffix("").name
+            con.create_table(f"tpc{suite}_{table_name}", con.read_parquet(path))
+
+    def _transform_tpc_sql(self, parsed, *, suite, leaves):
+        def add_catalog_and_schema(node):
+            if isinstance(node, sg.exp.Table) and (name := node.name) in leaves:
+                res = node.__class__(this=sg.to_identifier(f"tpc{suite}_{name}"))
+                return res
+            return node
+
+        return parsed.transform(add_catalog_and_schema)
+
+    def _tpc_table(self, name: str, benchmark: Literal["h", "ds"]):
+        if not getattr(self, f"supports_tpc{benchmark}"):
+            pytest.skip(
+                f"{self.name()} backend does not support testing TPC-{benchmark.upper()}"
+            )
+        return self.connection.table(f"tpc{benchmark}_{name}")
 
 
 @pytest.fixture(scope="session")
